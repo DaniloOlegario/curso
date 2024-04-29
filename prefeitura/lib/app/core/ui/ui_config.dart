a5dffee077a9b4f919d130faa943fa255573f0c8
DaniloOlegario <danilo_olegario@outlook.com>
2024-04-29 13:51:17 -0300

    alteração cor app

--- prefeitura/lib/app/core/ui/ui_config.dart
+++ prefeitura/lib/app/core/ui/ui_config.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-final class UiConfig{
+final class UiConfig {
   UiConfig._();
 
-  static const primaryColor = Color(0XFF29166F);
+  static const primaryColor = Color.fromARGB(255, 65, 30, 192);
   static const secondaryColor = Color(0XFFDA251C);
 
   static final theme = ThemeData();
-
 }
